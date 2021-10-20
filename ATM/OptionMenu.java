7f08288257011f8bed649d53d74ca636a7ba73fc
KangerDrew <kangdrew95@gmail.com>
2021-10-19 23:08:25 -0700

    More setup for OOP project

--- ATM/OptionMenu.java
+++ ATM/OptionMenu.java
@@ -3,3 +3,22 @@ import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
 
+public class OptionMenu extends Account {
+    Scanner menuInput = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    HashMap<Integer, Integer> data = new HashMap<>();
+
+    public void getLogin() throws IOException {
+        int x = 1;
+        do {
+            try {
+                data.put()
+            }
+
+
+        }
+
+    }
+
+}
